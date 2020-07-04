4e1f8ac08e39de9a108cb74d7c917bd75ba323c9
Ivan Alexandrov <radniforg@yandex.ru>
2020-07-05 02:05:52 +0300

    Search function added

--- VKinder.py
+++ VKinder.py
@@ -1,19 +1,19 @@
 from VK_TOKEN import token_confirmation
 from VK_TOKEN import user_confirmed
-from User_decomposition import user_interests
-from User_decomposition import user_element_weight
+import User_decomposition as ud
 from pprint import pprint
-from data_procession import profile_pictures
+import data_procession as dp
 import json
 
 
 
 if __name__ == '__main__':
-    TOKEN = '3641a1ff8ec80f2277ae1a29b0e7cc18054e6f07c367e621f2eee4c59dc8de6cf9afd5291a638752c6ca2'
+    TOKEN = '505deb0216ef6df79205599159c94a7ffc0a37b306281e8f33245280b0ba57b9d187713faebf732f23cfd'
     APP_ID =  7527992
     user = 6293784
     token_confirmation(APP_ID, TOKEN)
-    user_confirmed(6293784, TOKEN)
-    # pprint(user_interests(user, TOKEN))
-    # pprint(user_element_weight())
-    pprint(profile_pictures(user, TOKEN))
+    user_confirmed(user, TOKEN)
+    # pprint(ud.user_interests(user, TOKEN))
+    # pprint(ud.user_element_weight())
+    values = dp.data_preparation(ud.user_interests(user, TOKEN))
+    pprint(dp.user_search(values, TOKEN))
--- data_procession.py
+++ data_procession.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import time
 
+search_list_keys = ['activities', 'books', 'city', 'faculty_name', 'games', 'home_town', 'interests', 'movies',
+                    'music', 'occupation', 'religion', 'religion_2', 'tv']
 
 def profile_pictures(user_id, token):
     response_id = requests.get(
@@ -27,3 +30,55 @@ def profile_pictures(user_id, token):
         raw.append(temp_dictionary)
     raw.sort(reverse = True, key = lambda x: (x['like'], x['date']))
     return raw[:5]
+
+def search_hint(token):
+    response_id = requests.get(
+        'https://api.vk.com/method/search.getHints',
+        params={
+            'access_token': token,
+            'q': 'Tokio',
+            'limit': 9,
+            'search_global': 1,
+            'v': 5.99
+        }
+    )
+    search_data = response_id.json()
+    return search_data
+
+def user_search(search_queue, token):
+    giant_id_list = []
+    if type(search_queue) is not list:
+        temp_list = [search_queue]
+        search_queue = temp_list
+    for current_search in search_queue:
+        print(current_search)
+        response_id = requests.get(
+            'https://api.vk.com/method/users.search',
+            params={
+                'access_token': token,
+                'q': current_search,
+                'v': 5.99
+            }
+        )
+        search_data = response_id.json()['response']['items']
+        time.sleep(1)
+        id_list = []
+        for user in search_data:
+            try:
+                giant_id_list.index(user['id'])
+            except ValueError:
+                giant_id_list.append(user['id'])
+    return giant_id_list
+
+def data_preparation(elements):
+    #elements - выходные данные user_interests (user_decomposition.py)
+    total_search_values = []
+    for key, element in elements.items():
+        if key in search_list_keys:
+            if type(element) is list:
+                for subelement in element:
+                    total_search_values.append(subelement)
+            else:
+                if element:
+                    total_search_values.append(element)
+    return total_search_values
