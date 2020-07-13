3c48ce452743b4f5b768a81cbeefaa532bc0c775
Ivan Alexandrov <radniforg@yandex.ru>
2020-07-13 18:16:02 +0300

    Обновлен бекап settings.py

--- test/settings_backup.py
+++ test/settings_backup.py
@@ -1,6 +1,6 @@
 app_id = 7527992
 token = "069421845935feeb9f9ed4080073d85c0829cae1694e83bbc9a0affa65638090c68ba442ad8b49edbf1e7"
-user = "6293784"
+user = "184441185"
 standart_matrix = {"age_limit": "5", "age_difference": 2,
   "sex_preference": 1, "activities": 1, "books": 9, "city": 0, "common_count": 2, "faculty_name": 1,
   "games": 9, "home_town": 2, "interests": 9, "life_main": 6, "movies": 0, "music": 0, "occupation": 1,
@@ -26,3 +26,4 @@ translator = {'age_limit': 'Разница в возрасте',
               'tv': 'Значимость совпадения вкусов в сериалах и мультипликациях',
               'alcohol': 'Значимость несовпадения взглядов на алкоголь',
               'smoking': 'Значимость несовпадения взглядов на курение'}
+sql = {'database': 'vkinder', 'username': 'vinder', 'password': 'vinder'}
